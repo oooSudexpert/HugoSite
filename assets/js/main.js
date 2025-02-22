ba76e1e2a64c495fca1210dd7cfc981ecf9b6770
Stepan Gorshenin <gsv.tlt@gmail.com>
2025-02-22 12:58:33 +0400

    Add contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@
 //export { default as Alert } from './src/alert.js'
 //export { default as Button } from './src/button.js'
 //export { default as Carousel } from './src/carousel.js'
-export { default as Collapse } from './src/collapse.js'
+//export { default as Collapse } from './src/collapse.js'
 //////export { default as Dropdown } from './src/dropdown.js'
 //export { default as Modal } from './src/modal.js'
 //export { default as Offcanvas } from './src/offcanvas.js'
@@ -28,44 +28,67 @@ export { default as Collapse } from './src/collapse.js'
  * --------------------------------------------------------------------------
  */
 
+(function () {
+  'use strict'
 
-(() => {
-    'use strict'
-  
-    const btnTitle = 'Copy to clipboard'
-    const btnEdit = 'Edit on StackBlitz'
-  
-    const btnHtml = [
-      '<div class="bd-code-snippet">',
-      '  <div class="bd-clipboard">',
-      '    <button type="button" class="btn-clipboard">',
-      '      <svg class="bi" role="img" aria-label="Copy"><use xlink:href="#clipboard"/></svg>',
-      '    </button>',
-      '  </div>',
-      '</div>'
-    ].join('')
-  
-    document.addEventListener('click', (e) => {
-      alert(`Координаты: (${e.clientX},${e.clientY})`)
-    });
+  var successMessage = ':) Ваша заявка принята,\n сейчас наш специалист Вам перезвонит.';
+  var errorMessage = ':( Возникла непредвиденная ситуация.\n Ваша заявка не отправлена,\n попробуйте ещё раз.';
 
-    // Wrap programmatically code blocks and add copy btn.
-    document.querySelectorAll('.highlight')
-      .forEach(element => {
-        // Ignore examples made by shortcode
-        if (!element.closest('.bd-example-snippet')) {
-          element.insertAdjacentHTML('beforebegin', btnHtml)
-          element.previousElementSibling.append(element)
-        }
-      })
-  
-    /**
-     *
-     * @param {string} message
-     */
-    function sendMessage(message) {
+  const maskOptions = {
+    mask: '+{7} (000) 000-00-00'
+  };
 
-    }
+  var inputs = document.querySelectorAll('[name="Phone"]');
+  Array.prototype.slice.call(inputs)
+  .forEach(function (inputPhone) {
+    IMask(inputPhone, maskOptions); // https://imask.js.org/
+  });
+
+  var forms = document.querySelectorAll('.contact-form');
+
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+
+      form.addEventListener('submit', function (event) {
+        event.preventDefault()
+        event.stopPropagation()
+        if (form.checkValidity()) {
+          const city = form.dataset.city;
+          let phone = form.querySelector('[name="Phone"]').value.replace(/\D/g, '').substr(-10, 10);
+          let message;
+          const inputMessage = form.querySelector('[name="Message"]');
+          if (inputMessage !== null) {
+            message = inputMessage.value;
+          }
+          fetchMessage(city, phone, message).then((response) => {
+            if (response.status == 200) {
+              alert(successMessage);
+              form.classList.remove('was-validated');
+              form.reset();
+            } else {
+              alert(errorMessage);
+            }
+          });
+        } else {
+          form.classList.add('was-validated');          
+        }           
+      }, false)
+
+    })
+
+  async function fetchMessage(city, phone, message) {     
+
+    let data;
+    if (message == null) {
+      data = { city: city, phone: phone };
+    } else {
+      data = { city: city, phone: phone, text: message };      
+    }    
+    const params = new URLSearchParams(data).toString();
+    const url = `https://functions.yandexcloud.net/d4ehgtkcf9e302257mjc?${params}`;
   
-  })()
+    return await fetch(url);
+  }  
+
+})();
   
