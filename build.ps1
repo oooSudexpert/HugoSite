e979aef29a175c0a4bfa30c5a949bd3ce369cb63
Stepan Gorshenin <gsv.tlt@gmail.com>
2025-03-03 22:18:14 +0400

    build

--- build.ps1
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-hugo build --config .\hugo-prod-tlt.yml --environment production --cleanDestinationDir --minify
-hugo build --config .\hugo-prod-smr.yml --environment production --cleanDestinationDir --minify
-hugo build --config .\hugo-prod-msk.yml --environment production --cleanDestinationDir --minify
